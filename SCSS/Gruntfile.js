e640aed88270aacab4511ce72c66a43c515c44f6
Adam <adam.gedney@gmail.com>
2015-06-01 11:53:36 -0400

    UPDATED gruntfile for css style injection / hot reload

--- SCSS/Gruntfile.js
+++ SCSS/Gruntfile.js
@@ -14,15 +14,15 @@ module.exports = function(grunt) {
 
 
 //=========================================//
-// Project configuration.
+// Project configuration. ---
+// WordPress Ready --- ASSUMES the styles loaded are /style.css NOT /css/main.css
 //=========================================//
   grunt.initConfig({
 
     options: {
       // BASE_PATH variable (in WP this is the theme folder)
       // File assumes there is an scss and css folder in this directory
-      //BASE_PATH : './wp-content/themes/olympusat-kocka-child'
-      BASE_PATH : '.'
+      BASE_PATH : './wp-content/themes/olympusat-kocka-child'
     },
 
 
@@ -34,7 +34,7 @@ module.exports = function(grunt) {
       },
       dist: {
         files: {
-            '<%= options.BASE_PATH %>/css/main.css': '<%= options.BASE_PATH %>/scss/main.scss'
+            '<%= options.BASE_PATH %>/style.css': '<%= options.BASE_PATH %>/scss/main.scss'
         }
       }
     },//sass
@@ -44,13 +44,13 @@ module.exports = function(grunt) {
     cssmin: {
       options: {
         sourceMap: true,
-        target: '<%= options.BASE_PATH %>/css/'
+        target: '<%= options.BASE_PATH %>/'
       },
       minify: {
         expand: true,
-        cwd: '<%= options.BASE_PATH %>/css/',
+        cwd: '<%= options.BASE_PATH %>/',
         src: ['*.css', '!*.min.css'],
-        dest: '<%= options.BASE_PATH %>/css/',
+        dest: '<%= options.BASE_PATH %>/',
         ext: '.min.css'
       }
     },//cssmin
@@ -62,7 +62,7 @@ module.exports = function(grunt) {
         browsers: ['last 2 versions', 'safari 5', 'ie 6', 'ie 7', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4']
       },
       single_file: {
-        src: '<%= options.BASE_PATH %>/css/main.css'// Sets src and destination to the same
+        src: '<%= options.BASE_PATH %>/style.css'// Sets src and destination to the same
       }
     },
 
@@ -82,20 +82,34 @@ module.exports = function(grunt) {
       ]
     },
 
-
     //Watches files and folders for us
     watch: {
       sass: {
-        files: ['<%= options.BASE_PATH %>/{,*/}{,*/}*.{scss,sass}','<%= options.BASE_PATH %>/js/main.js'],
-        tasks: ['sass', 'cssmin', 'autoprefixer','jshint','uglify'],
+        files: ['<%= options.BASE_PATH %>/{,*/}{,*/}*.{scss,sass}'],
+        tasks: ['sass','cssmin', 'autoprefixer'],
         options: {
-          // Livereload requires the chrome extension LiveReload to be running
-          livereload: true
+          livereload: false // Livereload requires the chrome extension LiveReload to be running
           // to use on https pass path to cert
-          //key: grunt.file.read('path/to/ssl.key'),
-          //cert: grunt.file.read('path/to/ssl.crt')
+          // key: grunt.file.read('path/to/ssl.key'),
+          // cert: grunt.file.read('path/to/ssl.crt')
         }
-      }//sass
+      },//sass
+
+      js :{
+        files: ['<%= options.BASE_PATH %>/js/main.js'],
+        tasks: ['jshint','uglify'],
+        options: {
+          livereload: true
+        }
+      },
+
+      css:{
+        files : ['<%= options.BASE_PATH %>/style.css'],
+        tasks : [],
+        options: {
+          livereload: true
+        }
+      }
     }// watch
   //=========================================//
   });//grunt.initConfig
